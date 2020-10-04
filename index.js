450a40e28962079806f73c53fcbf5d3024ec3c96
Maxime BLANC <maximeblanc.dev@gmail.com>
2020-10-04 15:16:43 +0200

    create a basic readme

--- index.js
+++ index.js
@@ -1,17 +1,21 @@
-const twig = require('twig');
-const util = require('util');
+const twig = require("twig");
+const util = require("util");
 
 const renderFile = util.promisify(twig.renderFile);
 
 /**
  * Give the ability to use Twig template engine in Koa
  * @param {object} config
- * @param {string} views - the views folder path
- * @param {object} data - the data to pass to each view
+ * @param {string} config.views - the views folder path
+ * @param {object} config.data - the data to pass to each view
+ * @param {object} config.extension - the data to pass to each view
  */
 const twigMiddleware = (config) => async (ctx, next) => {
   function render(file, data) {
-    return renderFile(`${config.views}/${file}.html`, { ...config.data, ...data });
+    return renderFile(`${config.views}/${file}.${config.extension || "twig"}`, {
+      ...config.data,
+      ...data,
+    });
   }
 
   ctx.response.render = render;
@@ -21,4 +25,3 @@ const twigMiddleware = (config) => async (ctx, next) => {
 };
 
 module.exports = twigMiddleware;
-
